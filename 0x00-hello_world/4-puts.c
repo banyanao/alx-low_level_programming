9b63a5acfa211a695cbffb7744f3b94f2bb40b7a
Banyan Otchere <otcherebanyan@gmail.com>
2023-10-05 14:48:16 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
-int main (void)
+/**
+ * main - Entry point of program
+ *
+ * Return: (0) 
+ */
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle\n")
-	return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
