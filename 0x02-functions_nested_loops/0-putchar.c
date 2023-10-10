129bd241da66ab336ec9e553aa93a34fb9d304c0
Banyan Otchere <otcherebanyan@gmail.com>
2023-10-10 06:08:55 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+/**
+ * main- a program thet prints the text _putchar
+ *
+ * return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char text[] = "_putchar\n";
+
+	write(1, text, sizeof(text) - 1);
+	return (0);
+}
