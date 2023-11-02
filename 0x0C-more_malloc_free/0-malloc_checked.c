4ab9c9b4bfc44d236db679183ebd54a1f041283f
Banyan Otchere <otcherebanyan@gmail.com>
2023-11-02 07:12:10 -0700

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory
+ * @b:int
+ *
+ * Return: ptr
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+
+	return (ptr);
+}
